8a55dce9e185474913ccc49c9ca0e3045f8bfed2
Aleksandr Lisenko <aleksandrlisenko@Aleksandrs-MacBook-Pro-2.local>
2025-12-23 10:42:07 -0500

    Add Cask for pre-built binary installation

---
+++ Casks/infiniloom.rb
@@ -0,0 +1,25 @@
+cask "infiniloom" do
+  version "0.3.0"
+
+  on_intel do
+    url "https://github.com/Topos-Labs/infiniloom/releases/download/v#{version}/infiniloom-darwin-x64.tar.gz"
+    sha256 "155d94a5cc92c1c71b41db05c162408d89d2017d1c0a3d3a83902bf6024bbfcc"
+  end
+
+  on_arm do
+    url "https://github.com/Topos-Labs/infiniloom/releases/download/v#{version}/infiniloom-darwin-arm64.tar.gz"
+    sha256 "1cc4a5fb3e72597b76bb013090cca5bed119059d22471a0e7ff65f749c9ce014"
+  end
+
+  name "Infiniloom"
+  desc "High-performance repository context generator for LLMs"
+  homepage "https://github.com/Topos-Labs/infiniloom"
+  license "MIT"
+
+  binary "infiniloom"
+
+  zap trash: [
+    "~/.infiniloom",
+    "~/.config/infiniloom",
+  ]
+end
